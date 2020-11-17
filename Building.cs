e79e947c4179e28f041c26f35eb0689d30128b53
Ember Parr <ember.devops@gmail.com>
2020-11-17 11:53:19 -0600

    created Builing custom class

---
+++ Building.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Planner
+{
+    public class Building
+    {
+        private string _designer = "Ember Parr";
+        private DateTime _dateConstructed = DateTime.Now;
+        private string _address = "123 Main St.";
+        private string _owner = "Adam S.";
+
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+    }
+
+
+}
